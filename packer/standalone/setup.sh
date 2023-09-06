e52abc99ea7be6f201baf923b2217f6fab2ecb60
Adrian Navarro <adrian.navarro@visiondirect.co.uk>
2023-09-06 15:44:45 +0200

    fix setup.sh

--- packer/standalone/setup.sh
+++ packer/standalone/setup.sh
@@ -3,9 +3,9 @@
 echo "Install Jenkins stable release"
 yum remove -y java
 sudo dnf update -y
-dnf install java-17-amazon-corretto-devel
+dnf install java-17-amazon-corretto-devel -y
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
-rpm --import http://pkg.jenkins.io/redhat-stable/jenkins.io.key
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum install -y jenkins
 chkconfig jenkins on
 service jenkins start
