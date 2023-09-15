5aa6d517c262593a3448b4cbde0afaa612fc1c93
Adrian Navarro <adrian.navarro@visiondirect.co.uk>
2023-09-15 16:19:27 +0200

    Remove redundancies

--- lambda/functions/movies-marketplace/src/environments/environment.production.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  name: 'production',
-  apiURL: 'https://rth65vizrb.execute-api.eu-west-3.amazonaws.com/production',
-};
--- lambda/functions/movies-marketplace/src/environments/environment.staging.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    name: 'staging',
-    apiURL: 'https://rth65vizrb.execute-api.eu-west-3.amazonaws.com/staging',
-};
