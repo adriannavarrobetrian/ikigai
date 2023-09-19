a5887a54818ec589a26a20b023fede8fbdb05380
Adrian Navarro <adrian.navarro@visiondirect.co.uk>
2023-09-19 12:58:46 +0200

    Refactor

--- jenkins-pipeline-deploy-to-eks/terraform/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "primuslearning-app"
-    region = "us-east-1"
-    key = "eks/terraform.tfstate"
-  }
-}
--- terraform/live/staging/services/webserver-cluster/main.tf
+++ terraform/live/staging/services/webserver-cluster/main.tf
@@ -7,166 +7,33 @@ terraform {
       version = "~> 4.0"
     }
   }
-
-  backend "s3" {
-
-    # This backend configuration is filled in automatically at test time by Terratest. If you wish to run this example
-    # manually, uncomment and fill in the config below.
-
-    bucket         = "terraform-state-ikigai"
-    key            = "staging/services/webserver-cluster/terraform.tfstate"
-    region         = "eu-west-2"
-    dynamodb_table = "terraform-locks-ikigai"
-    encrypt        = true
-
-  }
-}  
+}
 
 provider "aws" {
   region = "eu-west-2"
 }
 
-resource "aws_launch_configuration" "example" {
-  image_id        = "ami-007ec828a062d87a5"
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.instance.id]
-  key_name = "adriantest"
+module "webserver_cluster" {
 
-  # Render the User Data script as a template
-  user_data = templatefile("user-data.sh", {
-    server_port = var.server_port
-    db_address  = data.terraform_remote_state.db.outputs.address
-    db_port     = data.terraform_remote_state.db.outputs.port
-  })
+  # Since the terraform-up-and-running-code repo is open source, we're using an HTTPS URL here. If it was a private
+  # repo, we'd instead use an SSH URL (git@github.com:brikis98/terraform-up-and-running-code.git) to leverage SSH auth
+  source = "github.com/adriannavarrobetrian/ikigai-terraform-modules//services/webserver-cluster?ref=v1.0.0"
 
-  # Required when using a launch configuration with an auto scaling group.
-  lifecycle {
-    create_before_destroy = true
-  }
+  cluster_name           = var.cluster_name
+  db_remote_state_bucket = var.db_remote_state_bucket
+  db_remote_state_key    = var.db_remote_state_key
+
+  instance_type = "t2.micro"
+  min_size      = 2
+  max_size      = 2
 }
 
-resource "aws_autoscaling_group" "example" {
-  launch_configuration = aws_launch_configuration.example.name
-  vpc_zone_identifier  = data.aws_subnets.default.ids
+resource "aws_security_group_rule" "allow_testing_inbound" {
+  type              = "ingress"
+  security_group_id = module.webserver_cluster.alb_security_group_id
 
-  target_group_arns = [aws_lb_target_group.asg.arn]
-  health_check_type = "ELB"
-
-  min_size = 1
-  max_size = 10
-
-  tag {
-    key                 = "Name"
-    value               = "terraform-asg-example"
-    propagate_at_launch = true
-  }
-}
-
-resource "aws_security_group" "instance" {
-  name = var.instance_security_group_name
-
-  ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_lb" "example" {
-  name               = var.alb_name
-  load_balancer_type = "application"
-  subnets            = data.aws_subnets.default.ids
-  security_groups    = [aws_security_group.alb.id]
-}
-
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.example.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  # By default, return a simple 404 page
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "404: page not found"
-      status_code  = 404
-    }
-  }
-}
-
-resource "aws_lb_target_group" "asg" {
-  name     = var.alb_name
-  port     = var.server_port
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpc.default.id
-
-  health_check {
-    path                = "/"
-    protocol            = "HTTP"
-    matcher             = "200"
-    interval            = 15
-    timeout             = 3
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
-}
-
-resource "aws_lb_listener_rule" "asg" {
-  listener_arn = aws_lb_listener.http.arn
-  priority     = 100
-
-  condition {
-    path_pattern {
-      values = ["*"]
-    }
-  }
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.asg.arn
-  }
-}
-
-resource "aws_security_group" "alb" {
-  name = var.alb_security_group_name
-
-  # Allow inbound HTTP requests
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Allow all outbound requests
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-data "terraform_remote_state" "db" {
-  backend = "s3"
-
-  config = {
-    bucket = var.db_remote_state_bucket
-    key    = var.db_remote_state_key
-    region = "eu-west-2"
-  }
-}
-
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
+  from_port   = 12345
+  to_port     = 12345
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
 }
