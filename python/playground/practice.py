b10b90b314d40d160333a78cd2b99dc4fe4abf2a
Adrian Navarro <adrian.navarro@visiondirect.co.uk>
2023-11-16 17:15:24 +0100

    fix lint

--- python/playground/practice.py
+++ python/playground/practice.py
@@ -1,6 +1,5 @@
 # Exercise 10: Remove duplicates from a list and create a tuple and find the minimum and maximum number
 # Given:
-import requests
 # sample_list = [87, 45, 41, 65, 94, 41, 99, 94]
 sample_list = [87, 45, 41, 65, 94, 41, 99, 94]
 unique_list = list(set(sample_list))
